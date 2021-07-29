c0316d16a0d899c0be83249990c0db72bda915c1
Daniel-ND <darya_nikolskaya@mail.ru>
2021-07-29 09:14:44 +0000

    added cuckoo hashtable

--- common/dpcpp/cuckoo_hashtable.hpp
+++ common/dpcpp/cuckoo_hashtable.hpp
@@ -1,18 +1,13 @@
-#include <stdlib.h>
-#include <stdint.h>
 #include <time.h>
 #include <CL/sycl.hpp>
 
-using namespace std;
-using namespace sycl;
 
-#define EMPTY_KEY 2147483647
+//#define EMPTY_KEY 2147483647
 
 class join_build;
 class join_probe;
 
 struct Hasher {
-
     Hasher(size_t sz) {
         _sz = sz;
         p = possible_p[rand() % 14];
@@ -38,19 +33,9 @@ struct Hasher {
 };
 
 template <class Key, class Hash> class CuckooHashtable{
-    private:
-        sycl::global_ptr<Key> _keys;
-        //sycl::global_ptr<T> _vals;
-        size_t _size;
-        Hash _hasher1;
-        Hash _hasher2;
-        //static constexpr uint32_t elem_sz = CHAR_BIT * sizeof(uint32_t);
-        static const sycl::ONEAPI::memory_order default_memory_order = sycl::ONEAPI::memory_order::relaxed;
-        static const sycl::memory_scope default_memory_scope = sycl::memory_scope::system;
-        
     public:
-        explicit CuckooHashtable(size_t size, sycl::global_ptr<Key> keys, Hash hasher1, Hash hasher2):
-            _size(size), _keys(keys), _hasher1(hasher1), _hasher2(hasher2) {}
+        explicit CuckooHashtable(size_t size, sycl::global_ptr<Key> keys, Key empty_key, Hash hasher1, Hash hasher2):
+            _size(size), _keys(keys), _empty_key(empty_key), _hasher1(hasher1), _hasher2(hasher2) {}
         
         bool at(Key key) {
             if (_keys[_hasher1(key)] == key || _keys[_hasher2(key)] == key)
@@ -63,9 +48,9 @@ template <class Key, class Hash> class CuckooHashtable{
                 size_t pos[] = {_hasher1(key), _hasher2(key)};
 
                 for (size_t i = 0; i < 2; i++){
-                    Key expected = EMPTY_KEY;
+                    Key expected = _empty_key;
                     sycl::ONEAPI::atomic_ref<Key, default_memory_order, default_memory_scope,
-                                    access::address_space::global_space> atomic_data(_keys[pos[i]]);
+                                    sycl::access::address_space::global_space> atomic_data(_keys[pos[i]]);
                 
                     if(atomic_data.compare_exchange_strong(expected, key, default_memory_order, default_memory_scope)) {
                         return true;    
@@ -73,7 +58,7 @@ template <class Key, class Hash> class CuckooHashtable{
                 }
 
                 sycl::ONEAPI::atomic_ref<Key, default_memory_order, default_memory_scope,
-                                access::address_space::global_space> atomic_data(_keys[pos[0]]);
+                                sycl::access::address_space::global_space> atomic_data(_keys[pos[0]]);
             
                 key = atomic_data.exchange(key, default_memory_order, default_memory_scope);
             
@@ -82,4 +67,14 @@ template <class Key, class Hash> class CuckooHashtable{
 
             return false;
         }
+
+    private:
+    sycl::global_ptr<Key> _keys;
+    Key _empty_key;
+    //sycl::global_ptr<T> _vals;
+    size_t _size;
+    Hash _hasher1;
+    Hash _hasher2;
+    static const sycl::ONEAPI::memory_order default_memory_order = sycl::ONEAPI::memory_order::relaxed;
+    static const sycl::memory_scope default_memory_scope = sycl::memory_scope::system;
 };
