46a65f86291a75da6a71bc5c3d18f75b2a35cbec
Daniel-ND <darya_nikolskaya@mail.ru>
2021-08-02 10:33:16 +0000

    added test for at method

--- tests/cuckoo_hashtable_tests.cpp
+++ tests/cuckoo_hashtable_tests.cpp
@@ -59,6 +59,71 @@ TEST(CuckooHashTable, insert) {
   ASSERT_EQ(keys[2], 2);
 }
 
+TEST(CuckooHashtable, at) {
+  const size_t buf_size = 10;
+  const size_t output_size = 7;
+  size_t bitmask_sz = (buf_size / 32) ? (buf_size / 32) : 1;
+  
+  CuckooTest::Hasher1<buf_size> hasher1;
+  CuckooTest::Hasher2<buf_size> hasher2;
+
+  sycl::cpu_selector device_selector;
+  sycl::queue q(device_selector);
+
+  std::vector<uint32_t> keys(buf_size, EMPTY_KEY);
+  std::vector<uint32_t> vals(buf_size, 0);
+  std::vector<uint32_t> bitmask(bitmask_sz, 0);
+  std::vector<std::pair<uint32_t, bool>> out(output_size, {0, false});
+  
+  {
+    sycl::buffer<uint32_t> keys_buf(keys);
+    sycl::buffer<uint32_t> vals_buf(vals);
+    sycl::buffer<uint32_t> bitmask_buf(bitmask);
+    sycl::buffer<std::pair<uint32_t, bool>> out_buf(out);
+
+    q.submit([&](sycl::handler &h) {
+      auto keys_acc = keys_buf.get_access(h);
+      auto vals_acc = vals_buf.get_access(h);
+      auto bitmask_acc = bitmask_buf.get_access(h);
+      auto out_acc = out_buf.get_access(h);
+
+      h.single_task([=]() {
+        CuckooHashtable<uint32_t, uint32_t, CuckooTest::Hasher1<buf_size>, CuckooTest::Hasher2<buf_size>> 
+            ht(buf_size, keys_acc.get_pointer(), vals_acc.get_pointer(), 
+                    bitmask_acc.get_pointer(), hasher1, hasher2, EMPTY_KEY);
+        
+        ht.insert(5, 5, 0);
+        ht.insert(15, 15, 0);
+        ht.insert(2, 2, 0);
+        ht.insert(258, 258, 0);
+        ht.insert(6, 6, 0);
+
+        out_acc[0] = ht.at(5);
+        out_acc[1] = ht.at(4);
+        out_acc[2] = ht.at(15);
+        out_acc[3] = ht.at(2);
+        out_acc[4] = ht.at(258);
+        out_acc[5] = ht.at(259);
+        out_acc[6] = ht.at(6);
+
+      });
+    });
+
+  }
+
+  for (int i = 0; i < buf_size; i++)
+    std::cout << keys[i] << " " << vals[i] << "\n";
+  
+  ASSERT_EQ(out[0].first, 5);
+  ASSERT_EQ(out[1].second, false);
+  ASSERT_EQ(out[2].first, 15);
+  ASSERT_EQ(out[3].first, 2);
+  ASSERT_EQ(out[4].first, 258);
+  ASSERT_EQ(out[5].second, false);
+  ASSERT_EQ(out[6].first, 6);
+
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
