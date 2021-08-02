47fc5ea09bd84e9279bfa699cc7be51fdb00973e
Daniel-ND <darya_nikolskaya@mail.ru>
2021-08-02 11:50:02 +0000

    added unable insert test

--- common/dpcpp/cuckoo_hashtable.hpp
+++ common/dpcpp/cuckoo_hashtable.hpp
@@ -42,16 +42,24 @@ template <class Key, class Val, class Hasher1, class Hasher2> class CuckooHashta
         static constexpr uint32_t elem_sz = CHAR_BIT * sizeof(uint32_t);
         
     public:
-        explicit CuckooHashtable(const size_t size, sycl::global_ptr<Key> keys, sycl::global_ptr<Val> vals, sycl::global_ptr<uint32_t> bitmask, Hasher1 hasher1, Hasher2 hasher2, const Key EMPTY_KEY):
+        explicit CuckooHashtable(const size_t size, sycl::global_ptr<Key> keys, sycl::global_ptr<Val> vals, 
+                                sycl::global_ptr<uint32_t> bitmask, Hasher1 hasher1, Hasher2 hasher2, const Key EMPTY_KEY):
             _size(size), _keys(keys), _vals(vals), _bitmask(bitmask), _hasher1(hasher1), _hasher2(hasher2), _EMPTY_KEY(EMPTY_KEY){}
         
         const std::pair<Val, bool> at(Key key) {
-            auto[pos1, pos2] = {_hasher1(key), _hasher2(key)};
+            auto pos1 = _hasher1(key);
+            auto pos2 = _hasher2(key);
             if (_keys[pos1] == key)
                 return {_vals[pos1], true};
             if (_keys[pos2] == key)
                 return {_vals[pos2], true};
             return {{}, false};
+        }
+
+        const bool has(Key key) {
+             if (_keys[_hasher1(key)] == key || _keys[_hasher2(key)] == key)
+                return true;
+            return false;
         }
 
         bool insert(Key key, Val value, size_t cnt) {
--- tests/cuckoo_hashtable_tests.cpp
+++ tests/cuckoo_hashtable_tests.cpp
@@ -121,8 +121,59 @@ TEST(CuckooHashtable, at) {
   ASSERT_EQ(out[4].first, 258);
   ASSERT_EQ(out[5].second, false);
   ASSERT_EQ(out[6].first, 6);
-
 }
+
+TEST(CuckooHashtable, fails_to_insert) {
+  const size_t buf_size = 5;
+  const size_t output_size = 4;
+  size_t bitmask_sz = (buf_size / 32) ? (buf_size / 32) : 1;
+  
+  CuckooTest::Hasher1<buf_size> hasher1;
+  CuckooTest::Hasher2<buf_size> hasher2;
+
+  sycl::cpu_selector device_selector;
+  sycl::queue q(device_selector);
+
+  std::vector<uint32_t> keys(buf_size, EMPTY_KEY);
+  std::vector<uint32_t> vals(buf_size, 0);
+  std::vector<uint32_t> bitmask(bitmask_sz, 0);
+  std::array<bool, output_size> out;
+  std::fill_n(out.begin(), output_size, false);
+  
+  //std::vector<bool> out(output_size, false);
+  
+  {
+    sycl::buffer<uint32_t> keys_buf(keys);
+    sycl::buffer<uint32_t> vals_buf(vals);
+    sycl::buffer<uint32_t> bitmask_buf(bitmask);
+    sycl::buffer<bool> out_buf(out);
+
+    q.submit([&](sycl::handler &h) {
+      auto keys_acc = keys_buf.get_access(h);
+      auto vals_acc = vals_buf.get_access(h);
+      auto bitmask_acc = bitmask_buf.get_access(h);
+      auto out_acc = out_buf.get_access(h);
+
+      h.single_task([=]() {
+        CuckooHashtable<uint32_t, uint32_t, CuckooTest::Hasher1<buf_size>, CuckooTest::Hasher2<buf_size>> 
+            ht(buf_size, keys_acc.get_pointer(), vals_acc.get_pointer(), 
+                    bitmask_acc.get_pointer(), hasher1, hasher2, EMPTY_KEY);
+        
+        out_acc[0] = ht.insert(5, 5, 0);
+        out_acc[1] = ht.insert(15, 15, 0);
+        out_acc[2] = ht.insert(20, 20, 0);
+        out_acc[3] = ht.insert(0, 0, 0);
+      });
+    });
+
+  }
+  
+  ASSERT_EQ(out[0], true);
+  ASSERT_EQ(out[1], true);
+  ASSERT_EQ(out[2], false);
+  ASSERT_EQ(out[3], false);
+}
+
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
