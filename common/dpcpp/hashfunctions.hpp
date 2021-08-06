9c5fc1646fd2bcda71b66e47cd1fda20da53db9d
Daria Nikolskaia <51816130+Daniel-ND@users.noreply.github.com>
2021-08-06 19:44:54 +0500

    Cuckoo Hashtable & Cuckoo Hashtable tests (#5)
    * Fixed dplscan due to compilation error
    
    * added cuckoo hashtable
    
    * added cuckoo hashtable
    
    * added values to cuckoo hashtable
    
    * + insert cuckoo hashtable test
    
    * changed method at()
    
    * added test for at method
    
    * added unable insert test
    
    * concatenate hashfunctions
    
    * remove commented parts
    
    * make at and has methods const
    
    * fix has method
    
    * remove cnt from method params
    
    * update lock & unlock
    
    * remove EMPTY_KEY from params
    
    * changed random
    
    * + parallel_insertion test
    
    * remove comment

---
+++ common/dpcpp/hashfunctions.hpp
@@ -0,0 +1,45 @@
+#include <random>
+
+struct PolynomialHasher {
+    PolynomialHasher(size_t sz) {
+        _sz = sz;
+        
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<int> dist(0, 13);
+        p = possible_p[dist(gen)];
+    }
+
+    size_t operator()(const uint32_t &v) const {
+        uint32_t v_copy = v;
+        int res = 0;
+        int pow_p = p;
+        while(v_copy > 0){
+            res += ((v_copy % 10) * pow_p) % _sz;
+            res = res % _sz;
+            pow_p *= p;
+            v_copy /= 10;
+        }
+        return res;
+    }
+    private:
+        size_t _sz;
+        int p;
+        const int possible_p[14] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43};
+};
+
+template <size_t Size> struct StaticSimpleHasher {
+  size_t operator()(const uint32_t &v) const { return v % Size; }
+};
+
+template <size_t Size, size_t Offset> struct StaticSimpleHasherWithOffset {
+  size_t operator()(const uint32_t &v) const { return (v % Size + Offset) % Size; }
+};
+
+template <class Integral> struct SimpleHasher {
+  SimpleHasher(size_t sz) : _sz(sz) {}
+  size_t operator()(const Integral &v) const { return v % _sz; }
+
+private:
+  size_t _sz;
+};
