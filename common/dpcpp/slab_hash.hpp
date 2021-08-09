1bc4e7500a4f022fd9c6d8dacb5891cb9398be5e
kuldanvit <kuldanvit@gmail.com>
2021-08-09 13:45:21 +0000

    +refactoring

--- common/dpcpp/slab_hash.hpp
+++ common/dpcpp/slab_hash.hpp
@@ -8,25 +8,22 @@
 
 namespace SlabHash {
 constexpr size_t SUBGROUP_SIZE = 16;
-constexpr size_t SLAB_SIZE_MULTIPLIER = 64;
+constexpr size_t SLAB_SIZE_MULTIPLIER = 16;
 constexpr size_t SLAB_SIZE = SLAB_SIZE_MULTIPLIER * SUBGROUP_SIZE;
 
-constexpr size_t CLUSTER_SIZE = 2048;
+constexpr size_t CLUSTER_SIZE = 20480;
 
-constexpr size_t BUCKETS_COUNT = 512;
+constexpr size_t BUCKETS_COUNT = 1024;
 
 constexpr size_t EMPTY_UINT32_T = std::numeric_limits<uint32_t>::max();
 
-
-template <size_t A, size_t B, size_t P>
-struct DefaultHasher {
+template <size_t A, size_t B, size_t P> struct DefaultHasher {
   size_t operator()(const uint32_t &k) {
     return ((A * k + B) % P) % BUCKETS_COUNT;
   };
 };
 
-template <typename T>
-struct SlabNode {
+template <typename T> struct SlabNode {
   SlabNode(T el) {
     for (int i = 0; i < SLAB_SIZE; i++) {
       data[i] = el;
@@ -37,67 +34,49 @@ struct SlabNode {
   sycl::device_ptr<SlabNode<T>> next = nullptr;
 };
 
-template <typename T>
-struct SlabList {
+template <typename T> struct SlabList {
   SlabList() = default;
-  SlabList(T empty, sycl::queue &q) {
-    auto tmp = sycl::device_ptr<SlabNode<T>>(
-        sycl::malloc_device<SlabNode<T>>(1, q));
-
-    q.parallel_for(1, [=](auto &i) {
-       *(tmp + i) = SlabNode<T>(empty);
-     }).wait();
-
-    root = tmp;
-  }
-
-  void clear(sycl::queue &q) { sycl::free(root, q); }
 
   sycl::device_ptr<SlabNode<T>> root;
 };
 
-template <typename T>
-struct HeapMaster {
+template <typename T> struct HeapMaster {
   HeapMaster(sycl::queue &q) : _q(q) {
     _heap = sycl::malloc_device<SlabNode<T>>(CLUSTER_SIZE, q);
     _head = _heap;
+    sycl::device_ptr<uint32_t> tmp_lock = sycl::malloc_device<uint32_t>(1, q);
+
+    q.single_task([=]() { *tmp_lock = 0; });
+
+    _lock = tmp_lock;
   }
 
   ~HeapMaster() { sycl::free(_heap, _q); }
 
   sycl::device_ptr<SlabNode<T>> malloc_node() {
     sycl::device_ptr<SlabNode<T>> ret;
-    while (sycl::atomic<uint32_t>(sycl::global_ptr<uint32_t>(&_lock))
+    while (sycl::atomic<uint32_t,
+                        sycl::access::address_space::global_device_space>(_lock)
                .fetch_or(1)) {
     }
     ret = _head;
     _head++;
-    sycl::atomic<uint32_t>(sycl::global_ptr<uint32_t>(&_lock)).fetch_and(0);
+    sycl::atomic<uint32_t, sycl::access::address_space::global_device_space>(
+        _lock)
+        .fetch_and(0);
     return ret;
   }
 
-  uint32_t _lock = 0;
+  sycl::device_ptr<uint32_t> _lock = nullptr;
   sycl::device_ptr<SlabNode<T>> _heap;
   sycl::device_ptr<SlabNode<T>> _head;
   sycl::queue &_q;
 };
 
-
-template <typename T>
-struct AllocAdapter {
+template <typename T> struct AllocAdapter {
   AllocAdapter(size_t bucket_size, T empty, sycl::queue &q) : _q(q), _heap(q) {
-    _data.resize(bucket_size);
+    _data.assign(bucket_size, SlabList<T>());
     _lock.assign(ceil((float)bucket_size / sizeof(uint32_t)), 0);
-
-    for (auto &e : _data) {
-      e = SlabList<T>(empty, q);
-    }
-  }
-
-  ~AllocAdapter() {
-    for (auto &e : _data) {
-      e.clear(_q);
-    }
   }
 
   std::vector<SlabList<T>> _data;
@@ -106,8 +85,7 @@ struct AllocAdapter {
   sycl::queue &_q;
 };
 
-template <typename K, typename T, typename Hash>
-class SlabHashTable {
+template <typename K, typename T, typename Hash> class SlabHashTable {
 public:
   SlabHashTable() = default;
   SlabHashTable(K empty, Hash hasher,
@@ -125,7 +103,11 @@ public:
     _val = val;
 
     if (_ind == 0) {
-      _iter = (_lists + _hasher(key))->root;
+      if ((_lists + _hasher(key))->root == nullptr) {
+        alloc_node((_lists + _hasher(key))->root);
+      } else {
+        _iter = (_lists + _hasher(key))->root;
+      }
     }
     sycl::group_barrier(_gr);
 
@@ -140,7 +122,8 @@ public:
 
         sycl::group_barrier(_gr);
       }
-      if (_ind == 0) alloc_node();
+      if (_ind == 0)
+        alloc_node(_prev->next);
       sycl::group_barrier(_gr);
     }
   }
@@ -167,29 +150,31 @@ public:
   }
 
 private:
-  void alloc_node() {
+  void alloc_node(sycl::device_ptr<SlabNode<std::pair<K, T>>> &src) {
     lock();
-    if (_prev->next == nullptr) {
-      _prev->next = _heap.malloc_node();
-      *_prev->next = SlabNode<std::pair<K, T>>({_empty, T()});
+    if (src == nullptr) {
+      auto tmp = _heap.malloc_node();
+      *tmp = SlabNode<std::pair<K, T>>({_empty, T()}); //!!!!!!
+      src = tmp;
     }
     unlock();
-    _iter = _prev->next;
-
+    _iter = src;
   }
 
   void lock() {
     auto tmp = _hasher(_key);
     int i = 0;
-    while (sycl::atomic<uint32_t>((_lock + (tmp / (CHAR_BIT * sizeof(uint32_t)))))
-               .fetch_or(1 << (tmp % (CHAR_BIT * sizeof(uint32_t)))) & (1 << (tmp % (CHAR_BIT * sizeof(uint32_t))))) {
+    while (
+        sycl::atomic<uint32_t>((_lock + (tmp / (CHAR_BIT * sizeof(uint32_t)))))
+            .fetch_or(1 << (tmp % (CHAR_BIT * sizeof(uint32_t)))) &
+        (1 << (tmp % (CHAR_BIT * sizeof(uint32_t))))) {
     }
   }
 
   void unlock() {
     auto tmp = _hasher(_key);
     sycl::atomic<uint32_t>((_lock + (tmp / (CHAR_BIT * sizeof(uint32_t)))))
-        .fetch_and(~(1 << (tmp %(CHAR_BIT * sizeof(uint32_t)))));
+        .fetch_and(~(1 << (tmp % (CHAR_BIT * sizeof(uint32_t)))));
   }
 
   bool insert_in_node() {
@@ -216,13 +201,14 @@ private:
     for (int j = 0; j < SUBGROUP_SIZE; j++) {
       if (cl::sycl::group_broadcast(_gr, find, j)) {
         K tmp_empty = _empty;
-        bool done = _ind == j ? sycl::ext::oneapi::atomic_ref<
-                                    K, sycl::ext::oneapi::memory_order::acq_rel,
-                                    sycl::ext::oneapi::memory_scope::device,
-                                    sycl::access::address_space::global_space>(
-                                    _iter->data[i].first)
-                                    .compare_exchange_strong(tmp_empty, _key)
-                              : false;
+        bool done = _ind == j
+                        ? sycl::ext::oneapi::atomic_ref<
+                              K, sycl::ext::oneapi::memory_order::acq_rel,
+                              sycl::ext::oneapi::memory_scope::device,
+                              sycl::access::address_space::global_device_space>(
+                              _iter->data[i].first)
+                              .compare_exchange_strong(tmp_empty, _key)
+                        : false;
         sycl::group_barrier(_gr);
         if (done) {
           _iter->data[i].second = _val;
@@ -245,7 +231,6 @@ private:
       find = ((_iter->data[i].first) == _key);
       sycl::group_barrier(_gr);
       total_found = sycl::any_of_group(_gr, find);
-
 
       if (total_found) {
         find_in_subgroup(find, i);
