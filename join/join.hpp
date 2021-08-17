7c038c3793e52285b62e77e62b402e3390c4f57e
kuldanvit <kuldanvit@gmail.com>
2021-08-17 14:24:53 +0000

    join back

---
+++ join/join.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "common/common.hpp"
+
+class Join : public Dwarf {
+public:
+  Join();
+  void run(const RunOptions &opts) override;
+  void init(const RunOptions &opts) override;
+
+private:
+  void _run(const size_t buffer_size, Meter &meter);
+};
