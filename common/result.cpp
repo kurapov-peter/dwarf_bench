fb765dad3fe7188296bf4f7784eda25c85292bc3
Petr Kurapov <petr.kurapov@gmail.com>
2022-02-10 17:45:22 +0300

    Add multi-stage results and fix groupby file format

--- common/result.cpp
+++ common/result.cpp
@@ -1,15 +1,20 @@
 #include "result.hpp"
 #include <fstream>
+#include <sstream>
 
 std::ostream &operator<<(std::ostream &os, const Result &res) {
   return res.print_to_stream(os);
 }
 
 std::ostream &Result::print_to_stream(std::ostream &os) const {
-  os << "Kernel duration: " << ((double)kernel_time) / 1000.0 << " us\n"
+  os << "Kernel duration: " << kernel_time.count() / 1000.0 << " us\n"
      << "Host duration:   " << host_time.count() << " us\n";
 
   return os;
+}
+
+std::vector<Duration> Result::get_reported_timings_list() const {
+  return {host_time, kernel_time};
 }
 
 std::ostream &HashJoinResult::print_to_stream(std::ostream &os) const {
@@ -19,6 +24,19 @@ std::ostream &HashJoinResult::print_to_stream(std::ostream &os) const {
      << "Probe time: " << probe_time.count() << " us\n";
 
   return os;
+}
+
+std::ostream &GroupByAggResult::print_to_stream(std::ostream &os) const {
+  Result::print_to_stream(os);
+
+  os << "Group stage time: " << group_by_time.count() << " us\n"
+     << "Reduce stage time: " << reduction_time.count() << " us\n";
+
+  return os;
+}
+
+std::vector<Duration> GroupByAggResult::get_reported_timings_list() const {
+  return {host_time, group_by_time, reduction_time};
 }
 
 MeasureResults::const_iterator MeasureResults::begin() const {
@@ -38,13 +56,29 @@ void MeasureResults::write_csv(const std::string &filename) const {
   bool exists = std::ifstream(filename).good();
   std::ofstream of(filename, std::ios::app);
   if (of.is_open()) {
-    if (!exists)
-      of << "device_type,buf_size_bytes,host_time_ms,kernel_time_ms\n";
+    if (!exists) {
+      of << "device_type,buf_size_bytes," << header_ << "\n";
+    }
     for (const auto &res : results_) {
       of << res.params.at("device_type") << ","
          << std::stoi(res.params.at("buf_size")) * 4 << ",";
-      of << res.result->host_time.count() / 1000.0 << ","
-         << ((double)res.result->kernel_time) / (1000.0 * 1000.0) << "\n";
+      auto measurements = res.result->get_reported_timings_list();
+      // todo: refactor me
+      auto transformer = [](const Duration &d) {
+        return std::chrono::duration_cast<std::chrono::microseconds>(d)
+                   .count() /
+               1000.0;
+      };
+
+      std::ostringstream tmp;
+      for (auto &e : measurements) {
+        tmp << transformer(e) << ",";
+      }
+      auto tmp_s = tmp.str();
+      if (!tmp_s.empty()) {
+        tmp_s.resize(tmp_s.size() - 1);
+      }
+      of << tmp_s << "\n";
     }
   } else {
     throw std::runtime_error("Could not open the file at " + filename);
--- common/result.hpp
+++ common/result.hpp
@@ -15,9 +15,11 @@ struct Result {
   size_t bytes = 0;
   size_t iterations = 0;
   size_t bytes_per_iteration = 0;
-  unsigned long kernel_time = 0;
+  Duration kernel_time;
   Duration host_time;
   bool valid = true;
+
+  virtual std::vector<Duration> get_reported_timings_list() const;
 
 protected:
   virtual std::ostream &print_to_stream(std::ostream &os) const;
@@ -30,6 +32,13 @@ struct HashJoinResult : public Result {
   std::ostream &print_to_stream(std::ostream &os) const override;
 };
 
+struct GroupByAggResult : public Result {
+  Duration group_by_time;
+  Duration reduction_time;
+  std::vector<Duration> get_reported_timings_list() const override;
+  std::ostream &print_to_stream(std::ostream &os) const override;
+};
+
 std::ostream &operator<<(std::ostream &os, const Result &res);
 
 struct DwarfRunResult {
@@ -37,20 +46,24 @@ struct DwarfRunResult {
   std::unique_ptr<Result> result;
 };
 
+static constexpr auto default_report_header = "host_time_ms,kernel_time_ms";
 using SingleRunResults = std::vector<DwarfRunResult>;
 class MeasureResults {
 public:
   using const_iterator = SingleRunResults::const_iterator;
-  MeasureResults(const std::string &name) : name_(name) {}
+  MeasureResults(const std::string &name)
+      : name_(name), header_(default_report_header) {}
 
   void add_result(DwarfParams params, std::unique_ptr<Result> result);
 
   const_iterator begin() const;
   const_iterator end() const;
 
+  void set_report_header(std::string &&header);
   void write_csv(const std::string &filename) const;
 
 private:
   SingleRunResults results_;
   const std::string name_;
+  std::string header_;
 };
