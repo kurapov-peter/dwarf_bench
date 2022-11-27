bf56a90197af0868c51f0ad780117d054650361c
Daniil Kulikov <kuldanvit@gmail.com>
2022-11-27 21:11:08 +0000

    reconfigure examples and add dbench usage

---
+++ example/bench_usage/main.cpp
@@ -0,0 +1,19 @@
+#include "bench.hpp"
+#include <iostream>
+
+int main() {
+    DwarfBench::DwarfBench db;
+
+    DwarfBench::RunConfig rc = {
+        .device = DwarfBench::DeviceType::CPU,
+        .inputSize = 1024,
+        .iterations = 10,
+        .dwarf = "Radix",
+    };
+
+    auto results = db.makeMeasurements(rc);
+
+    for (auto &result: results) {
+        std::cout << "RESULT: " << result.dataSize << ' ' << result.microseconds << std::endl;
+    }
+}
